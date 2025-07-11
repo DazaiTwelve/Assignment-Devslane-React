ae4d76a49eae730d9e3eccc9e5bd15ad10a01a95
Dazai Twelve <98841464+DazaiTwelve@users.noreply.github.com>
2025-07-11 16:40:18 +0530

    Replace products with skateboard catalog

--- src/TableRow.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TableRow({num,idx}){
-    return (
-        <div className='text-indigo-600 text-xl font-medium'>
-            {num} x {idx} ={num*idx}
-        </div>
-    )
-}
